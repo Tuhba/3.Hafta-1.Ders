3f6e5bf03ddb07e0aa52008f4d71d34f5b61ed7f
Tuhba <ozkayikcitugba@gmail.com>
2023-10-02 13:24:07 +0300

    Add project files.

---
+++ Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw.Entities
+{
+    internal class Review : EntityBase<Guid>
+    {
+        public string Text {  get; set; }
+        public int Rating { get; set; } 
+    }
+}
